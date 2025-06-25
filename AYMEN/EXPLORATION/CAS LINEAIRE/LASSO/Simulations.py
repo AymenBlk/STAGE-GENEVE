Fix Lasso simulation parameters and update lambda calculation
- Reverted lambda assignment in SimulationLassoOracleIsta to use _get_lambda method instead of a hardcoded value.
- Commented out beta and beta_hat print statements for cleaner output during simulations.
- Adjusted the variance in the normal distribution for Y_sim in qut_lasso_oracle function to use sigma squared instead of sigma.

AYMEN/EXPLORATION/CAS LINEAIRE/LASSO/Simulations.py
@@ -72,8 +72,7 @@ class SimulationLassoOracleIsta:
 
             y, X, beta = self._generate_data(s, seed = self.seed + i)
 
-            #lmbda = self._get_lambda(X)
-            lmbda = 0.1
+            lmbda = self._get_lambda(X)
             L = np.linalg.norm(X.T @ X, ord=2) / self.n
             beta_hat = np.zeros(self.p)
 
@@ -93,8 +92,9 @@ class SimulationLassoOracleIsta:
 
             if self.verbose:
                 print(f"\t |{i+1}| Simulation {i+1}/{self.simu_iter} :")
-                print(f"\t\t beta : {beta}")
-                print(f"\t\t beta estimé : {beta_hat}")
+                #print(f"\t\t beta : {beta}")
+                #print(f"\t\t beta estimé : {beta_hat}")
+                print(f"\t\t Lambda : {lmbda}")
                 print(f"\t\t Score :")
                 for key in score_tmp:
                     print(f"\t\t\t{key} : {score_tmp[key]}")

AYMEN/EXPLORATION/CAS LINEAIRE/OUTILS/linear_tools.py
@@ -354,7 +354,7 @@ def qut_lasso_oracle(
     rng = np.random.default_rng(seed)
     Lambda = []
     for _ in range(M):
-        Y_sim = rng.normal(0, sigma, size=n)
+        Y_sim = rng.normal(0, sigma**2, size=n)
         lambda_0 = np.linalg.norm(X.T @ Y_sim / n, ord=np.inf)
         Lambda.append(lambda_0)
     lambda_qut = np.quantile(Lambda, 1 - alpha)